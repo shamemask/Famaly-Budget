e8ed48df7b3d4249caa10a781909aa8a0240186e
Daniil <77277774+shamemask@users.noreply.github.com>
2025-04-22 16:37:46 +0300

    feat: Добавил swagger

--- budget/urls.py
+++ budget/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# Настройка Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Famaly budget API",
+        default_version="v1",
+        description="An application for managing the family budget.",
+        contact=openapi.Contact(email="shamemask@ya.ru"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("transactions.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
